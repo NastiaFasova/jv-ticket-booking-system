5076a107f1952a4fdfb62208b0e20fec1fc9246d
NastiaFasova <anastasiia.fasova@lifecell.com.ua>
2025-10-15 18:18:30 +0300

    +

--- src/main/java/mate/academy/TicketBookingSystem.java
+++ src/main/java/mate/academy/TicketBookingSystem.java
@@ -1,12 +1,25 @@
 package mate.academy;
 
+import java.util.concurrent.Semaphore;
+
 public class TicketBookingSystem {
+    private Semaphore semaphore;
+    private int totalSeats;
 
     public TicketBookingSystem(int totalSeats) {
-
+        semaphore = new Semaphore(totalSeats);
     }
 
     public BookingResult attemptBooking(String user) {
-        return null;
+        BookingResult bookingResult1;
+        try {
+            semaphore.acquire();
+            bookingResult1 = new BookingResult(user, true, "Booking successful.");
+            totalSeats--;
+            semaphore.release();
+        } catch (InterruptedException e) {
+            return new BookingResult(user, false, "No seats available.");
+        }
+        return bookingResult1;
     }
 }
